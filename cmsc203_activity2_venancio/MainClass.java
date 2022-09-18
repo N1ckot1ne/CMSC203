0d2566f2c277219180d1e1520a837cf4b89d81ee
Venancio, Nicko A <106007314+N1ckot1ne@users.noreply.github.com>
2022-09-18 22:26:12 +0800

    Add files via upload

---
+++ cmsc203_activity2_venancio/MainClass.java
@@ -0,0 +1,16 @@
+/**
+ * MainClass
+ */
+public class MainClass {
+    public static void main(String[] args) 
+    {
+        int a = 3;
+        int b = 5;
+        int result = b + b;
+
+        System.out.println("int 1 = " + a);
+        System.out.println(" int 2 = " + b);
+
+        System.out.println(" RESULT: 5 + 5 = " + result);
+    }
+}
